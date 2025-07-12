cbfa8777abf24df97bb09d4f2a81808e41cb2137
sanespinoz <san.espinoz@gmail.com>
2025-07-12 18:08:04 -0300

    Corrijo ruta de audio para GitHub Pages

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ const score = document.querySelector(".score--value");
 const finalScore = document.querySelector(".final-score > span");
 const menu = document.querySelector(".menu-screen");
 const buttonPlay = document.querySelector(".btn-play");
-const audio = new Audio("../assets/audio.mp3");
+//const audio = new Audio("../assets/audio.mp3");
+const basePath = location.hostname.includes("github.io")
+  ? "/js-snake-game/"
+  : "/";
+const audio = new Audio(`${basePath}assets/audio.mp3`);
 
 console.log(score);
 const size = 30;
